a21967b5694464f4268a400a048cde8dbd87a9ed
zillako <devzillako@gmail.com>
2024-10-03 16:59:43 +0900

    apply domain

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://zillako.vercel.app",
+  link: "https://www.kojinyoung.com", // "https://zillako.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
