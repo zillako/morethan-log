73b7ec15bf8004684aa57d90eefad7e411046938
zillako <devzillako@gmail.com>
2024-10-04 22:51:53 +0900

    header menu changed

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "Resume", to: "/resume" }]
   return (
     <StyledWrapper className="">
       <ul>
